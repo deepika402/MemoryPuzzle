e420ef23d61e6e311f1df1f5f5d1af1fecdc3313
deepika402 <ddhanaraj420@gmail.com>
2020-01-09 10:13:28 +0000

    boardheight=5

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import random, pygame, sys
 from pygame.locals import *
 
 FPS = 30 # frames per second, the general speed of the program
-WINDOWWIDTH = 640 # size of window's width in pixels
+WINDOWWIDTH = 840 # size of window's width in pixels
 WINDOWHEIGHT = 480 # size of windows' height in pixels
-REVEALSPEED = 8 # speed boxes' sliding reveals and covers
-BOXSIZE = 40 # size of box height & width in pixels
-GAPSIZE = 10 # size of gap between boxes in pixels
-BOARDWIDTH = 10 # number of columns of icons
-BOARDHEIGHT = 7 # number of rows of icons
+REVEALSPEED = 4 # speed boxes' sliding reveals and covers
+BOXSIZE = 80 # size of box height & width in pixels
+GAPSIZE = 20 # size of gap between boxes in pixels
+BOARDWIDTH = 5 # number of columns of icons
+BOARDHEIGHT = 5 # number of rows of icons
 assert (BOARDWIDTH * BOARDHEIGHT) % 2 == 0, 'Board needs to have an even number of boxes for pairs of matches.'
 XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
 YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
+
 
 
 #            R    G    B
@@ -42,6 +43,7 @@ ALLSHAPES = (DONUT, SQUARE, DIAMOND, LINES, OVAL)
 assert len(ALLCOLORS) * len(ALLSHAPES) * 2 >= BOARDWIDTH * BOARDHEIGHT, "Board is too big for the number of shapes/colors defined."
 
 def main():
+  user = str(input("Enter the player name:"))
     global FPSCLOCK, DISPLAYSURF
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
@@ -277,6 +279,7 @@ def gameWonAnimation(board):
 
 
 def hasWon(revealedBoxes):
+  print(FPSCLOCK)
     # Returns True if all the boxes have been revealed, otherwise False
     for i in revealedBoxes:
         if False in i:
